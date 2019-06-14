5765110de7d96850592cbead1e610c91c0f1eb10
autoandshare <autoandshare@gmail.com>
2019-06-14 14:43:37 +0800

    check if videoRenderer is null

--- src/main/java/autoandshare/headvr/activity/VideoActivity.java
+++ src/main/java/autoandshare/headvr/activity/VideoActivity.java
@@ -181,8 +181,10 @@ public class VideoActivity extends GvrActivity implements
         super.onPause();
         Log.i(TAG, "onPause()");
 
-        videoRenderer.pause();
-        videoRenderer.savePosition();
+        if (videoRenderer != null) {
+            videoRenderer.pause();
+            videoRenderer.savePosition();
+        }
     }
 
     @Override
